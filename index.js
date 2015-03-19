d144c30d227705b96c643df9a42d22b50cf176d2
Erik Schwartz <erik@erikschwartz.net>
2015-03-18 20:47:38 -0400

    Init Schwartzography points

---
+++ index.js
@@ -0,0 +1,37 @@
+Chicago, Illinois	-87.6244212	41.8755546
+Lexington, Kentucky	-84.4970393	38.0464066
+Ypsilanti, MI	-83.6129939	42.2411499
+Nashville, TN	-86.7743419	36.1622257
+Harker Heights, Texas	-97.6597377	31.0835102
+Buenos Aires, Argentina	-58.4459789	-34.612869
+Colonia del Sacramento, Uruguay	-57.8433679	-34.4698592
+Panama City, Panama	-79.5344539	8.9707433
+Playa Grande, Costa Rica	-85.846758	10.3383024
+Red Rock, Texas	-97.4461058	29.9596656
+Austin, Texas	-97.7436995	30.2711286
+Houston, TX	-95.52668411	29.974291
+Lafayette, Louisiana	-92.0198427	30.2240897
+New Orleans, Louisiana	-90.0701156	29.9499323
+Jackson, Mississippi	-90.16206489	32.4104385
+Chattanooga, Tennessee	-85.3096801	35.0456297
+Nashville, Tennessee	-86.7743419	36.1622257
+Lexington, Kentucky	-84.4970393	38.0464066
+Chicago, Illinois	-87.6244212	41.8755546
+Ypsilanti, Michigan	-83.6129939	42.2411499
+Lexington, Kentucky	-84.4970393	38.0464066
+Lihue, Hawaii	-159.3711111	21.9811111
+Lawai, Hawaii	-159.5094444	21.9219444
+Hanalei Bay, Hawaii	-159.5091667	22.21
+Honolulu, Hawaii	-157.8556764	21.304547
+Hilo, Hawaii	-155.1003753	19.6854458
+Kailua-Kona, Hawaii	-155.9966291	19.6418457
+Madison, Wisconsin	-89.3837613	43.074761
+Lake Geneva, Wisconsin	-88.4334301	42.5916836
+Chicago, IL	-87.6244212	41.8755546
+Lexington, KY	-84.4970393	38.0464066
+Ann Arbor, Michigan	-83.7312291	42.2681569
+Columbus, Ohio	-83.0007065	39.9622601
+Erie, Pennsylvania	-80.085239	42.129461
+Philadelphia, Pennsylvania	-75.163789	39.952335
+Washington, D.C.
+San Francisco, California	-122.4629897	37.7647993
